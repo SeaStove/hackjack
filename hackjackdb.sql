1af5c346192fb7d5c1d480375abc61705969e532
Apple <apple@wirelessprvnat-172-17-126-124.near.illinois.edu>
2016-02-20 06:01:52 -0600

    6 am changes

--- hackjackdb.sql
+++ hackjackdb.sql
@@ -1,42 +1,31 @@
--- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: hackjackdb
--- ------------------------------------------------------
--- Server version	5.5.46-0ubuntu0.14.04.2
+DROP TABLE IF EXISTS 'game';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE 'game'(
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'status' varchar(50),
+  PRIMARY KEY (id)
+)
 
---
--- Table structure for table `Probability`
---
-
-DROP TABLE IF EXISTS `Probability`;
+DROP TABLE IF EXISTS `probability`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Probability` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ace` int(11) DEFAULT NULL,
-  `one` int(11) DEFAULT NULL,
-  `two` int(11) DEFAULT NULL,
-  `three` int(11) DEFAULT NULL,
-  `four` int(11) DEFAULT NULL,
-  `five` int(11) DEFAULT NULL,
-  `six` int(11) DEFAULT NULL,
-  `seven` int(11) DEFAULT NULL,
-  `eight` int(11) DEFAULT NULL,
-  `nine` int(11) DEFAULT NULL,
-  `ten` int(11) DEFAULT NULL,
-  `TotalFields` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+CREATE TABLE `probability` (
+  `id` int(11),
+  `ace` int(11) DEFAULT 4,
+  `one` int(11) DEFAULT 4,
+  `two` int(11) DEFAULT 4,
+  `three` int(11) DEFAULT 4,
+  `four` int(11) DEFAULT 4,
+  `five` int(11) DEFAULT 4,
+  `six` int(11) DEFAULT 4,
+  `seven` int(11) DEFAULT 4,
+  `eight` int(11) DEFAULT 4,
+  `nine` int(11) DEFAULT 4,
+  `ten` int(11) DEFAULT 16,
+  `total` int(11) DEFAULT 52,
+  FOREIGN KEY (id)
+    REFERENCES game(id)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -44,7 +33,7 @@ CREATE TABLE `Probability` (
 -- Dumping data for table `Probability`
 --
 
-LOCK TABLES `Probability` WRITE;
+LOCK TABLES `probability` WRITE;
 /*!40000 ALTER TABLE `Probability` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Probability` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -53,23 +42,17 @@ UNLOCK TABLES;
 -- Table structure for table `TrueCount`
 --
 
-DROP TABLE IF EXISTS `TrueCount`;
+DROP TABLE IF EXISTS `trueCount`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `TrueCount` (
-  `CardID` int(11) NOT NULL AUTO_INCREMENT,
-  `ace` int(11) DEFAULT '1',
-  `one` int(11) DEFAULT '-1',
-  `two` int(11) DEFAULT '-1',
-  `three` int(11) DEFAULT '-1',
-  `four` int(11) DEFAULT '-1',
-  `five` int(11) DEFAULT '-1',
-  `six` int(11) DEFAULT '-1',
-  `seven` int(11) DEFAULT '0',
-  `eight` int(11) DEFAULT '0',
-  `nine` int(11) DEFAULT '0',
-  `ten` int(11) DEFAULT '1',
-  PRIMARY KEY (`CardID`)
+CREATE TABLE `trueCount` (
+  `id` int(11),
+  `total` int(11) DEFAULT '52',
+  `decks` int(11) DEFAULT '1',
+  `running` int(11) DEFAULT '0'
+  FOREIGN KEY (id)
+    REFERENCES game(id)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -77,7 +60,7 @@ CREATE TABLE `TrueCount` (
 -- Dumping data for table `TrueCount`
 --
 
-LOCK TABLES `TrueCount` WRITE;
+LOCK TABLES `trueCount` WRITE;
 /*!40000 ALTER TABLE `TrueCount` DISABLE KEYS */;
 /*!40000 ALTER TABLE `TrueCount` ENABLE KEYS */;
 UNLOCK TABLES;
