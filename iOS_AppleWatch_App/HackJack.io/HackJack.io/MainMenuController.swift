a0a222414406018aa11cee0a3c89dd3a88c75792
Apple <rochovian@gmail.com>
2016-02-21 04:56:27 -0600

    fixing name

---
+++ iOS_AppleWatch_App/HackJack.io/HackJack.io/MainMenuController.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenuController.swift
+//  HackJack.io
+//
+//  Created by Apple on 2/20/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuController: NSObject {
+
+}
---
+++ iOS_AppleWatch_App/HackJack.io/HackJack.io/MainMenuViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MainMenuViewController.swift
+//  HackJack.io
+//
+//  Created by Apple on 2/20/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+import UIKit
+import WatchConnectivity
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getTrue(sender: AnyObject) {
+        
+            let request = NSMutableURLRequest(URL: NSURL(string: "http://hackjack.cloudapp.net/api/true/get.php")!)
+            request.HTTPMethod = "POST"
+            let postString = "id=1"
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+                guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                    print("error=\(error)")
+                    return
+                }
+                
+                if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {           // check for http errors
+                    print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                    print("response = \(response)")
+                }
+                
+                let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print("responseString = \(responseString)")
+            }
+            task.resume()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
